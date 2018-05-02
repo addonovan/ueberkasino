1c1ae7768b83ff3d7abcddab412001e8e3ce4a96
Austin D. Donovan <austin@addonovan.com>
2018-05-02 10:11:45 -0500

    Finished implementation of remaining strategies (#40)
    * Implemented conservative strategy
    
    * Created lookup table for basic strategy
    
    * Implemented basic lookup strategy
    
    * Realized all strategies are constant time
    
    Yeah, because there's the upper bound on the number of cards in a hand,
    there's always a constant number of steps that we could solve any
    strategy in (not all strategies, necessarily, but each strategy
    individually).
    
    * Fixed compile errors
    
    using didn't work like I'd wanted, and neither did double array
    initializers :(

--- src/strategy.cpp
+++ src/strategy.cpp
@@ -1,4 +1,5 @@
 #include <strategy.hpp>
+#include <misc.hpp>
 
 namespace uc
 {
@@ -6,9 +7,7 @@ namespace uc
     net::Action
     RecklessStrategy::process( const net::Game& game ) const
     {
-        // hide unused parameter warnings
         ( void )( game );
-
         return net::Action::hitting;
     }
 
@@ -16,26 +15,79 @@ namespace uc
     ManualStrategy::process( const net::Game& game ) const
     {
         ( void )( game );
-
         return net::Action::idle;
+    }
+    
+    net::Action
+    BasicStrategy::process( const net::Game& game ) const
+    {
+        // trying to cut out as much boilerplate as possible here, just to make
+        // the chart easier to read/understand
+        using std::array;
+        using net::Action;
+#define H Action::hitting
+#define S Action::standing
+#define D Action::doubling
+
+        // the constant lookup table describing what action should be performed
+        static const array< array< Action, 10 >, 19 > lookup = {{
+            // Dealer's Visibile Count           Player's Count
+            //2  3  4  5  6  7  8  9  10 A       ==============
+            { H, H, H, H, H, H, H, H, H, H }, // 2
+            { H, H, H, H, H, H, H, H, H, H }, // 3
+            { H, H, H, H, H, H, H, H, H, H }, // 4
+            { H, H, H, H, H, H, H, H, H, H }, // 5
+            { H, H, H, H, H, H, H, H, H, H }, // 6
+            { H, H, H, H, H, H, H, H, H, H }, // 7
+            { H, H, H, H, H, H, H, H, H, H }, // 8
+            { H, D, D, D, D, H, H, H, H, H }, // 9
+            { D, D, D, D, D, D, D, D, H, H }, // 10
+            { D, D, D, D, D, D, D, D, D, D }, // 11
+            { H, H, S, S, S, H, H, H, H, H }, // 12
+            { S, S, S, S, S, H, H, H, H, H }, // 13
+            { S, S, S, S, S, H, H, H, H, H }, // 14
+            { S, S, S, S, S, H, H, H, H, H }, // 15
+            { S, S, S, S, S, H, H, H, H, H }, // 16
+            { S, S, S, S, S, H, H, H, H, H }, // 17
+            { S, S, S, S, S, S, S, S, S, S }, // 18
+            { S, S, S, S, S, S, S, S, S, S }, // 19
+            { S, S, S, S, S, S, S, S, S, S }  // 20
+        }};
+
+#undef D
+#undef S
+#undef H
+
+        // calculate the player's count
+        auto player = value_of( to_hand( game.p[ game.active_player ].cards ) );
+        auto dealer = value_of( to_hand( game.dealer_cards ) );
+
+        // because both the player and dealer's counts can start at a min of 2,
+        // we need to subtract 2 to get to the first position
+        player -= 2;
+        dealer -= 2;
+
+        // opting to use `at( index )` instead of `[ index ]` here because it
+        // will do bounds checking for us, ensuring that nothing funky went on
+        return lookup.at( player ).at( dealer );
     }
 
     net::Action
     ConservativeStrategy::process( const net::Game& game ) const
     {
-        // TODO actual implementation
-        ( void )( game );
+        auto count = value_of( to_hand( game.p[ game.active_player ].cards ) );
 
-        return net::Action::standing;
-    }
-
-    net::Action
-    BasicStrategy::process( const net::Game& game ) const
-    {
-        // TODO actual implementation
-        ( void )( game );
-
-        return net::Action::standing;
+        // if we could bust by getting a ten-point-card, then we won't ever hit,
+        // we can safely disregard and eleven-valued-ace in this case because if
+        // that were to cause us to bust, then it'd be valued at a 1 instead.
+        if ( count + 10 > 21 )
+        {
+            return net::Action::standing;
+        }
+        else
+        {
+            return net::Action::hitting;
+        }
     }
 
 }
